5d71b22fd4cc7c9bb5523097c7b32f05c4a5bdc9
Giovanni Bordiga <gio.61192@gmail.com>
2020-11-12 15:25:34 +0100

    Add test data for testing the whole script

--- tests/test_latex_subfigs_combiner.py
+++ tests/test_latex_subfigs_combiner.py
@@ -1,5 +1,19 @@
 from latex_subfigs_combiner import __version__
+import subprocess
+import glob
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_script():
+
+    # execute script with test parameters
+    job = subprocess.run(["combine-subfigs", "./tests/data/latex-project/paper.tex", "--target_dir",
+                          "./tests/data/combined-figures"])
+    assert job.returncode == 0
+
+    # check number of figures produced
+    numOutputFigs = len(glob.glob("./tests/data/combined-figures/*.pdf"))
+    assert numOutputFigs == 5
