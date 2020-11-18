2aa560667ad4feb4121c637d406d01f3c0de854a
Giovanni Bordiga <gio.61192@gmail.com>
2020-11-18 23:58:29 +0100

    Fix bug executing latexmk in wrong directory and refacoring using pahlib

--- latex_subfigs_combiner/combine_subfigures.py
+++ latex_subfigs_combiner/combine_subfigures.py
@@ -5,6 +5,7 @@ import glob
 import os
 import re
 import subprocess
+from pathlib import Path
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
@@ -29,9 +30,9 @@ parser.add_argument("--version", "-v", action="version",
                     version="%(prog)s v{version}".format(version=__version__), help="Shows current version.")
 
 args = parser.parse_args()
-texFile = args.tex_source       # input tex file
-targetPath = args.target_dir    # target directory where final figures will be collected
-prefix = args.prefix            # prefix for figure filenames
+texFilePath = Path(args.tex_source)     # input tex file
+targetPath = Path(args.target_dir)      # target directory where final figures will be collected
+prefix = args.prefix                    # prefix for figure filenames
 
 
 def extractCompositeFigureStrings(latexString):
@@ -49,13 +50,13 @@ def extractCompositeFigureStrings(latexString):
     return figureStrings
 
 
-def parseTexFile():
+def parseTexFile(texFilePath):
     """
     Parses the TeX file, returning the list of composite figures and the preamble.
     """
 
     # read in tex file
-    with open(texFile) as latex:
+    with open(texFilePath) as latex:
         latexString = latex.read()
 
     # remove all commented code
@@ -70,7 +71,7 @@ def parseTexFile():
     return preambleString, figureStrings
 
 
-def compileLatex(preambleString, figureStrings, tmpFilename):
+def compileLatex(preambleString, figureStrings, compilationPath, tmpFilename):
     """
     Compiles the auxiliary TeX file with one figure per page.
     """
@@ -80,14 +81,15 @@ def compileLatex(preambleString, figureStrings, tmpFilename):
 
     # compile auxiliary tex file with one figure per page
     # TODO: would be nice to compile only those figures that have changes since last compilation (can be done by storing the list figureStrings)
-    with open(tmpFilename + ".tex", "w") as tmpTex:
+    tmpTexPath = compilationPath / (tmpFilename + ".tex")
+    with open(tmpTexPath, "w") as tmpTex:
         tmpTex.write(preambleString + beginDocument + "\n\\pagebreak\n".join(figureStrings) + endDocument)
 
-    # compile auxiliary tex files
-    subprocess.run(["latexmk", "-pdf", "-silent", tmpFilename])
+    # compile auxiliary tex file
+    subprocess.run(["latexmk", "-pdf", "-silent", "-cd", str(tmpTexPath)])
 
 
-def createCompositeFigures(figNames, tmpFilename):
+def createCompositeFigures(targetPath, figNames, compilationPath, tmpFilename):
     """
     Creates the composite figures in targetPath and clean up auxiliary files.
     """
@@ -97,23 +99,26 @@ def createCompositeFigures(figNames, tmpFilename):
         os.mkdir(targetPath)
     except OSError:
         # clean up target directory
-        for file in glob.glob(targetPath + "/*"):
+        for file in targetPath.iterdir():
             os.remove(file)
 
     # extract figures from pages
-    tmpPDF = PdfFileReader(open(tmpFilename + ".pdf", "rb"))
+    tmpPDFPath = Path(tmpFilename + ".pdf")
+    tmpPDF = PdfFileReader(open(tmpPDFPath, "rb"))
     for i in range(tmpPDF.numPages):
         # extract i-th page containing figure to crop
         output = PdfFileWriter()
         output.addPage(tmpPDF.getPage(i))
-        with open(tmpFilename + "_page.pdf", "wb") as outputStream:
+        pagePath = Path(tmpFilename + "_page.pdf")
+        with open(pagePath, "wb") as outputStream:
             output.write(outputStream)
         # crop figure and move it into target directory
-        subprocess.run(["pdfcrop", tmpFilename + "_page.pdf", targetPath + "/" + figNames[i]])
+        subprocess.run(["pdfcrop", str(pagePath), str(targetPath / figNames[i])])
 
     # clean up auxiliary files
     for file in glob.glob(tmpFilename + "*"):
         os.remove(file)
+    os.remove(compilationPath / (tmpFilename + ".tex"))
 
 
 def main():
@@ -122,15 +127,15 @@ def main():
     """
 
     # parse tex file extracting composite figures and preable
-    preambleString, figureStrings = parseTexFile()
+    preambleString, figureStrings = parseTexFile(texFilePath)
 
     # compile auxiliary tex file with one figure per page
     tmpFilename = "comb_fig_tmp"  # name of auxiliary files
-    compileLatex(preambleString, figureStrings, tmpFilename)
+    compileLatex(preambleString, figureStrings, texFilePath.parent, tmpFilename)
 
     # create composite figures
     figNames = [prefix + str(i + 1) + ".pdf" for i in range(len(figureStrings))]  # figure filenames for output
-    createCompositeFigures(figNames, tmpFilename)
+    createCompositeFigures(targetPath, figNames, texFilePath.parent, tmpFilename)
 
 
 # entrypoint for cli invocation
